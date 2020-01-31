1c7a91ef5f245a6dba6a0e0350ff13d8fc0dd178
hiro-forest <h.ginnoringokara41@icloud.com>
2020-01-31 16:19:35 +0900

    プロフィール作成、削除、編集

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,7 +4,7 @@ class Address < ApplicationRecord
 
   belongs_to :user
   validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/}
-  validates :prefecture, presence: true
-  validates :city, presence: true
-  validates :street, presence: true
+  validates :prefecture,  presence: true
+  validates :city,        presence: true
+  validates :street,      presence: true
 end
