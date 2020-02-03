43bbc28b1e74ed3d082f31be2b55078763ea707b
hiro-forest <h.ginnoringokara41@icloud.com>
2020-02-04 06:32:37 +0900

    微調整

--- db/migrate/20200125092453_add_column_to_hope.rb
+++ db/migrate/20200125092453_add_column_to_hope.rb
@@ -1,5 +1,5 @@
 class AddColumnToHope < ActiveRecord::Migration[6.0]
   def change
-    add_column :hopes, null: false, :desired_price, :integer
+    add_column :hopes, :desired_price, :integer, null: false
   end
 end
